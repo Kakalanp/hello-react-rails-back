5238202af2c9a338c4ba6e36ee28a3f118dc3867
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-07-23 14:29:49 -0500

    finished database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting:'Hello')
+Greeting.create(greeting:'Hola')
+Greeting.create(greeting:'konnichiwa')
+Greeting.create(greeting:'Ciao')
+Greeting.create(greeting:'Ni hao')
+Greeting.create(greeting:'Kaixo')
+Greeting.create(greeting:'Zadravo')
