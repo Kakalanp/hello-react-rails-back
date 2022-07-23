662432a1ba986c22f04dd0f5dca11734d277124a
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-07-23 14:17:27 -0500

    fixing linter eerrors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  def show
+    greetings = Greeting.all
+    index = Random.rand(0..4)
+    @greeting = greetings[index]
+    respond_to do |format|
+      format.html { render 'show' }
+      format.json { render json: @greeting }
+    end
+  end
+
+  def index; end
 end
